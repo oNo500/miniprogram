8a6183882a89cb7eb014ddf5b3d99b21465448a8
xiu <gaoxiu333@gmail.com>
2026-03-22 14:00:42 +0800

    chore(demo): add color classes to icon examples

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -11,11 +11,11 @@ export default function Index () {
     <View className='index'>
       <Text>Hello world!</Text>
       {/* iconify collection: lucide */}
-      <Text className='i-lucide-binary'></Text>
+      <Text className='i-lucide-binary text-red-400'></Text>
       {/* custom local SVGs: src/assets/icons/*.svg → i-custom-<name> */}
       <Text className='i-custom-github'></Text>
-      <Text className='i-custom-wechat'></Text>
-      <Text className='i-custom-alipay'></Text>
+      <Text className='i-custom-wechat text-green-500'></Text>
+      <Text className='i-custom-alipay text-blue-500'></Text>
     </View>
   )
 }
